840231bb8a313f637b362b307d478410309b400a
gbcwbz <cwbddzxx@gmail.com>
2015-08-21 19:22:43 +0800

    add client and server function

---
+++ socket/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+namespace socket
+{
+    class Program
+    {
+        private static byte[] result = new byte[1024];
+        private static int myPort = 8885;
+        static Socket serverSocket;
+        static void Main(string[] args)
+        {
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            serverSocket.Bind(new IPEndPoint(ip, myPort));
+            serverSocket.Listen(10);
+            Console.WriteLine("启动监听{0}成功", serverSocket.LocalEndPoint.ToString());
+            //通过Clientsocket发送数据
+            Thread myThread = new Thread(ListenClientConnect);
+            myThread.Start();
+            Console.ReadLine();
+
+        }
+
+        /// <summary>
+        /// 监听客户端连接
+        /// </summary>
+        private static void ListenClientConnect()
+        {
+            while(true)
+            {
+                Socket clientSocket = serverSocket.Accept();
+                clientSocket.Send(Encoding.ASCII.GetBytes("Server Say Hello"));
+                Thread receiveThread = new Thread(ReceiveMessage);
+                receiveThread.Start(clientSocket);
+            }
+
+        }
+
+        /// <summary>
+        /// 接收消息
+        /// </summary>
+        /// <param name="clientSocket"></param>
+        private static void ReceiveMessage(object clientSocket)
+        {
+            Socket myClientSocket = (Socket)clientSocket;
+            while(true)
+            {
+                try
+                {
+                    // 通过clientSocket接收数据
+                    int receiveNumber = myClientSocket.Receive(result);
+                    Console.WriteLine("接收客户端{0}消息{1}", myClientSocket.RemoteEndPoint.ToString(), Encoding.ASCII.GetString(result, 0, receiveNumber));
+
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    myClientSocket.Shutdown(SocketShutdown.Both);
+                    myClientSocket.Close();
+                    break;
+                }
+
+            }
+
+        }
+    }
+}
---
+++ socket_client/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+
+
+namespace socket_client
+{
+    class Program
+    {
+        private static byte[] result = new byte[1024];
+
+        static void Main(string[] args)
+        {
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                clientSocket.Connect(new IPEndPoint(ip, 8885)); // 配置服务器IP与端口
+                Console.WriteLine("连接服务器成功");
+
+
+            }
+            catch
+            {
+                Console.WriteLine("连接服务器失败，请按回车键退出！");
+                return;
+            }
+
+            int receiveLength = clientSocket.Receive(result);
+            Console.WriteLine("接收服务器消息：{0}", Encoding.ASCII.GetString(result, 0, receiveLength));
+            // 通过clientSocket发送数据
+            for(int i=0; i<10; i++)
+            {
+                try
+                {
+                    Thread.Sleep(1000);
+                    string sendMessage = "client send Message Hello" + DateTime.Now;
+                    clientSocket.Send(Encoding.ASCII.GetBytes(sendMessage));
+                    Console.WriteLine("向服务器发送消息：" + sendMessage);
+                }
+                catch
+                {
+                    clientSocket.Shutdown(SocketShutdown.Both);
+                    clientSocket.Close();
+                    break;
+                }
+
+            }
+
+            Console.WriteLine("发送完毕，按回车键退出");
+            Console.ReadLine();
+        }
+    }
+}
